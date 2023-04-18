5447a5b19900958fbce98de4ea79291f8b7bd442
Jannes Vandepitte <jannes.vandepitte@gmail.com>
2023-04-18 17:30:10 +0200

    Enable compression

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,15 +2,15 @@ import { NestFactory } from "@nestjs/core";
 import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.js";
+import compression from "compression";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.setGlobalPrefix("/api");
   app.use(cookieParser());
   app.use(graphqlUploadExpress({ maxFileSize: 100000, maxFiles: 10 }));
+  app.use(compression());
   await app.listen(3001);
 }
 
-// AppClusterService.clusterize(bootstrap);
 bootstrap();
