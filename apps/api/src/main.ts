3879045970c2d88a340644d7baf3abd120ad0294
Jannes Vandepitte <jannes.vandepitte@gmail.com>
2023-04-18 17:42:48 +0200

    revert compression

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,14 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 import graphqlUploadExpress from "graphql-upload/graphqlUploadExpress.js";
-import compression from "compression";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-  app.use(graphqlUploadExpress({ maxFileSize: 100000, maxFiles: 10 }));
-  app.use(compression());
+  app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   await app.listen(3001);
 }
 
